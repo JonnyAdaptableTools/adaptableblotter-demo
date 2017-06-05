9c17636c022236265fef3a0eb9a67c593c5b9cbe
Jonathan NAIM <jo.naim@adaptabletools.com>
2017-06-05 19:54:59 +0100

    change deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,66 @@
 #!/bin/sh
 
 folder=$1
+user=$2
+password=$3
 
 cd dist
 #find dist -type f -exec echo ftp://files.000webhost.com$folder{} \;
 # find /dist -type f -exec curl --user ${FTP_USER}:${FTP_PASSWORD} --ftp-create-dirs -T {} ftp://files.000webhost.com/public_html/{} \;
 
+filestoupload=""
+
 for file in **/**/**/*
 do
-    echo uploading ${file} to ftp://files.000webhost.com$folder${file}
-    curl -u ${FTP_USER}:${FTP_PASSWORD} -T ${file} ftp://files.000webhost.com$folder${file}
+    if [[ -f ${file} ]]
+    then
+        if [ -z ${filestoupload} ]
+        then
+            filestoupload=${file}
+        else
+            filestoupload=${filestoupload}","${file}
+        fi
+    fi
 done
+
+for file in **/**/*
+do
+    if [[ -f ${file} ]]
+    then
+        if [ -z ${filestoupload} ]
+        then
+            filestoupload=${file}
+        else
+            filestoupload=${filestoupload}","${file}
+        fi
+    fi
+done
+
+for file in **/*
+do
+    if [[ -f ${file} ]]
+    then
+        if [ -z ${filestoupload} ]
+        then
+            filestoupload=${file}
+        else
+            filestoupload=${filestoupload}","${file}
+        fi
+    fi
+done
+
+for file in *
+do
+    if [[ -f ${file} ]]
+    then
+        if [ -z ${filestoupload} ]
+        then
+            filestoupload=${file}
+        else
+            filestoupload=${filestoupload}","${file}
+        fi
+    fi
+done
+
+#echo $filestoupload
+curl -u ${user}:${password} --ftp-create-dirs -T "{"${filestoupload}"}" ftp://files.000webhost.com$folder --keepalive-time 60
