1cbb3fa2670054b73d7a44b2a04ed44b48809415
roblotter <roblotter1@gmail.com>
2019-05-07 15:29:53 +0300

    Add env var support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 // next.config.js
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
 const withFonts = require('next-fonts');
@@ -12,5 +14,24 @@ let nextConfig = withCSS(
 nextConfig = withTypescript(withFonts(nextConfig));
 
 module.exports = Object.assign({}, nextConfig, {
-  pageExtensions: ['jsx', 'js', 'tsx', 'ts']
+  pageExtensions: ['jsx', 'js', 'tsx', 'ts'],
+  webpack: (config, options) => {
+    config.plugins = config.plugins || [];
+
+    config.plugins = [
+      ...config.plugins,
+
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: false
+      })
+    ];
+
+    if (typeof nextConfig.webpack === 'function') {
+      return nextConfig.webpack(config, options);
+    }
+
+    return config;
+  }
 });
