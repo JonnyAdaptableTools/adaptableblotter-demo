5e462604c9c25bd9c7fb412dd8d77c7031895322
roblotter <roblotter1@gmail.com>
2019-05-10 19:16:59 +0300

    update

--- src/Helper.ts
+++ src/Helper.ts
@@ -75,16 +75,6 @@ export function manageDomDataSetSelectAndReturnSelectDataset(
   return selectedConfig;
 }
 
-
-
-
-
-
-
-
-
-
-
 export function generateRandomInt(minValue: number, maxValue: number): number {
   return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
 }
