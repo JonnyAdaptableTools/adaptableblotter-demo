0e79ebf6c4e83dd44483a7ca4ba01dcb93522d6b
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2019-05-28 17:58:39 +0300

    starting on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,13 +20,55 @@ export default () => {
   return (
     <MainPage pageTitle="AdaptableBlotter Demos">
       <div style={{ padding: 20 }}>
-        <h2>Home Page</h2>
+        <h3>Adaptable Blotter Demos</h3>
         <i>
-          Version:
+          Version:&nbsp;
           {version}&nbsp;&nbsp;({versiondate})
         </i>
-        <p>Will explain about the demos</p>
-        <img src={logo} width="54" height="34" />
+        <br />
+        <br />
+        <p>
+          The Adaptable Blotter is the market leading HTML5 DataGrid Solution.
+        </p>
+        <p>
+          It sits on top of (and integrates with) a number of HTML5 vendor
+          DataGrids including ag-Grid.{' '}
+        </p>
+        <p>
+          <b>The Adaptable Blotter</b>
+          <br /> AdaptableBlotter.JS is a powerful DataGrid add-on that
+          provides, out of the box, all the functionality that financial and
+          other advanced users require to be productive and efficient. And which
+          usually needs to be built on a bespoke basis for each project.
+          <br />
+          AdaptableBlotter.JS can be integrated with a number of different
+          underlying HTML5 grids, both vendor and open source.
+          <br />
+          AdaptableBlotter.JS works with any dataset, any Grid type, any Desk,
+          and in any Location
+          <br />
+          Adaptable Blotter.JS is fully data-agnostic and can work with any data
+          set you provide it with.
+          <br />
+        </p>
+        <b>Integration</b>
+        <br />
+        It is trivial to set up the Adaptable Blotter and can be done with a
+        single line of code.
+        <br />
+        See this{' '}
+        <a href="https://yg0we.codesandbox.io/" target="_blank">
+          Live Example
+        </a>{' '}
+        and download it and experiment with it yourself using this{' '}
+        <a
+          href="https://codesandbox.io/s/adaptableblotterbasic-yg0we"
+          target="_blank"
+        >
+          Sandbox
+        </a>
+        .
+        <br />
       </div>
     </MainPage>
   );
