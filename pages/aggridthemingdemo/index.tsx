e10cc8a2030059ea8ae570deb418f37f6e0f912e
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2019-05-29 19:53:58 +0300

    updating some page text

--- pages/aggridthemingdemo/index.tsx
+++ pages/aggridthemingdemo/index.tsx
@@ -8,15 +8,52 @@ export default () => {
       pageTitle={'AdaptableBlotter.JS ag-Grid Theming Demo'}
       description={
         <>
+          <h4>Theme Demo</h4>
           <p>
-            <b>Setting the Theme</b>
+            You can style and theme the Adaptable Blotter so that it matches
+            precisely your corporate styles and colours.
           </p>
           <p>
-            This example shows the Adaptable Blotter with the 'Dark Theme'
-            selected.
-            <br />
-            This theme can be changed by selecting a new value from the Theme
-            dropdown in the Toolbar (which we have made visible)
+            We currently use Bootstrap 3 and you can use any Bootstrap theme you
+            want instead. A release in the near future will allow for full style
+            management entirely through css and without needing to use Bootstrap
+            at all.
+          </p>
+          <p>
+            The Blotter ships with 2 themes ('Light' and 'Dark'). This example
+            shows the Adaptable Blotter with the 'Dark Theme' selected - use the
+            dropdown in the Theme toolbar to change the selection.
+          </p>
+          <p>
+            <b>Adaptable Blotter Help Resources:</b>{' '}
+            <a
+              href="https://adaptabletools.zendesk.com/hc/en-us/articles/360005168191-Theme-Picker"
+              target="_blank"
+            >
+              User Guide
+            </a>
+            ,{' '}
+            <a
+              href="https://adaptabletools.zendesk.com/hc/en-us/articles/360028587452-Theme-Config"
+              target="_blank"
+            >
+              Predefined Config
+            </a>
+            ,{' '}
+            <a
+              href="https://adaptabletools.zendesk.com/hc/en-us/articles/360028899491-Theme-API"
+              target="_blank"
+            >
+              Blotter API
+            </a>{' '}
+            and{' '}
+            <a
+              href="https://adaptabletools.zendesk.com/hc/en-us/articles/360009005211-Theme-Picker-FAQ"
+              target="_blank"
+            >
+              FAQ
+            </a>
+            .
           </p>
         </>
       }
