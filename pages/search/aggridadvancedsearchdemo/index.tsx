e810d8a1225ad90059588805275700899c234f77
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2019-05-29 13:55:43 +0300

    changes to search pages

--- pages/search/aggridadvancedsearchdemo/index.tsx
+++ pages/search/aggridadvancedsearchdemo/index.tsx
@@ -9,7 +9,59 @@ export default () => {
       description={
         <div>
           <h4>Advanced Search</h4>
-          <p>To Do</p>
+          <p>
+            Advanced Search is a very powerful function that can run searches
+            across mutliple columns which can then be named, saved and re-used.
+          </p>
+          <p>
+            It uses the Adaptable Blotter Query (used in many Functions) that
+            allows selections to be performed on a mix of Column Values, Filters
+            and Ranges.
+          </p>
+          <p>
+            In this example we are searching for any rows where the Invoiced
+            Cost is over $350 (<i>Range</i>), for Orders this year (
+            <i>Filter</i>) where the Employee is Janet, Margaret or Robert (
+            <i>Column Values</i>).
+          </p>
+          <p>
+            <b>Adaptable Blotter Help Resources:</b>{' '}
+            <a
+              href="https://adaptabletools.zendesk.com/hc/en-us/articles/360005167931-Advanced-Search"
+              target="_blank"
+            >
+              User Guide
+            </a>
+            ,{' '}
+            <a
+              href="https://adaptabletools.zendesk.com/hc/en-us/articles/360028900991-Advanced-Search-Config"
+              target="_blank"
+            >
+              Predefined Config
+            </a>
+            ,{' '}
+            <a
+              href="https://adaptabletools.zendesk.com/hc/en-us/articles/360028585192-Advanced-Search-API"
+              target="_blank"
+            >
+              Blotter API
+            </a>
+            ,{' '}
+            <a
+              href="https://adaptabletools.zendesk.com/hc/en-us/articles/360009005151-Quick-Search-FAQ"
+              target="_blank"
+            >
+              FAQ
+            </a>{' '}
+            and{' '}
+            <a
+              href="https://adaptabletools.zendesk.com/hc/en-us/articles/360009004951-Advanced-Search-FAQ"
+              target="_blank"
+            >
+              Queries
+            </a>
+            .
+          </p>
         </div>
       }
     />
--- pages/search/aggridquicksearchdemo/index.tsx
+++ pages/search/aggridquicksearchdemo/index.tsx
@@ -9,7 +9,51 @@ export default () => {
       description={
         <div>
           <h4>Quick Search</h4>
-          <p>To do</p>
+          <p>
+            Quick Search enables you to quickly find matching text across all{' '}
+            <b>visible</b> columns. You can use wildcards like '*' (starts with)
+            or '>' (greater than) to refine your search.
+          </p>
+          <p>
+            Quick Search Config allows you to set the style of matched cells and
+            whether to hide or show rows with no matching cells.
+          </p>
+          <p>
+            In this example we have chosen a darker yellow than the default
+            option, chosen to hide non matching rows and are searching for any
+            columns that start with 'g'.
+          </p>
+          <p>
+            <b>Adaptable Blotter Help Resources:</b>{' '}
+            <a
+              href="https://adaptabletools.zendesk.com/hc/en-us/articles/360005167911-Quick-Search"
+              target="_blank"
+            >
+              User Guide
+            </a>
+            ,{' '}
+            <a
+              href="https://adaptabletools.zendesk.com/hc/en-us/articles/360028901031-Quick-Search-Config"
+              target="_blank"
+            >
+              Predefined Config
+            </a>
+            ,{' '}
+            <a
+              href="https://adaptabletools.zendesk.com/hc/en-us/articles/360028585452-Quick-Search-API"
+              target="_blank"
+            >
+              Blotter API
+            </a>{' '}
+            and{' '}
+            <a
+              href="https://adaptabletools.zendesk.com/hc/en-us/articles/360009005151-Quick-Search-FAQ"
+              target="_blank"
+            >
+              FAQ
+            </a>
+            .
+          </p>
         </div>
       }
     />
