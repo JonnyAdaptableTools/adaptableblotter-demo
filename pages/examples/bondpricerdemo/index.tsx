09b4ad00fbcb0069aec3740cfafa3949ce0cc828
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2019-06-24 07:46:30 +0100

    some more demos

---
+++ pages/examples/bondpricerdemo/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import DynamicDemoPage from '../../../src/DynamicDemoPage';
+
+export default () => {
+  return (
+    <DynamicDemoPage
+      demo={import('../../../src/client/bondpricerdemo')}
+      pageTitle={'Bond Pricer Demo'}
+      description={
+        <div>
+          <h4>Bond Pricer Demo</h4>
+          <p>To do</p>
+        </div>
+      }
+    />
+  );
+};
---
+++ src/client/aggridmasterdetaildemo/demo.ts
@@ -0,0 +1,43 @@
+import * as Helper from '../../Helper';
+
+import AdaptableBlotter from 'adaptableblotter/agGrid';
+import 'adaptableblotter/base.css';
+import 'adaptableblotter/themes/light.css';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import { cloneDeep } from 'lodash';
+
+import '../../../DemoPage/aggriddemo.css';
+
+import { AdaptableBlotterOptions } from 'adaptableblotter/types';
+
+import { HelperAgGrid } from '../../HelperAgGrid';
+import { DummyDataHelper } from '../../DummyDataHelper';
+import predefinedConfig from './config';
+
+export default () => {
+  let helperAgGrid = new HelperAgGrid();
+  helperAgGrid.setUpAgGridLicence();
+
+  let dummyDataHelper = new DummyDataHelper();
+
+  const gridOptions = dummyDataHelper.getMasterGridOptionsFootball();
+
+  const blotterOptions: AdaptableBlotterOptions = {
+    primaryKey: 'OrderId',
+    userName: 'Demo User',
+    blotterId: 'Grouping Demo',
+    licenceKey: Helper.getdemolicencekey(),
+    vendorGrid: gridOptions,
+    predefinedConfig: predefinedConfig,
+  };
+
+  const blotterOptionsClone = cloneDeep(blotterOptions);
+  new AdaptableBlotter(blotterOptions);
+
+  return {
+    predefinedConfig,
+    blotterOptions: blotterOptionsClone,
+  };
+};
