3df644b9bc19693fc48f4c1e2a539df5dbc1a660
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2019-08-20 17:45:34 +0100

    fixing build

--- src/Helpers/HypergridDemoPage.tsx
+++ src/Helpers/HypergridDemoPage.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react';
 import Head from 'next/head';
 
-import logo from '../images/AdaptableBlotter.png';
+//import logo from '../images/AdaptableBlotter.png';
 
 type TypeProps = {
   pageTitle: string;
@@ -16,8 +16,6 @@ export default ({ pageTitle, children, description }: TypeProps) => {
         <title>{pageTitle}</title>
       </Head>
       <nav style={{ marginBottom: 0, display: 'flex', flexFlow: 'row' }}>
-        <img src={logo} height="50" />
-
         {description}
       </nav>
       <div id="adaptableBlotter" />
