6eae3a00ed260b2d48cb3fbc8986d185efa825cb
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2019-11-06 13:25:26 +0000

    updating theme

--- src/client/theme/aggridcustomthemedemo/demo.ts
+++ src/client/theme/aggridcustomthemedemo/demo.ts
@@ -2,22 +2,31 @@ import * as Helper from '../../../Helpers/Helper';
 
 import AdaptableBlotter from '@adaptabletools/adaptableblotter/agGrid';
 import '@adaptabletools/adaptableblotter/index.css';
-import './wimbledon.css';
+import '@adaptabletools/adaptableblotter/themes/dark.css';
+import './custometheme.css';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham-dark.css';
+import 'ag-grid-community/dist/styles/ag-theme-blue.css';
 
 import { cloneDeep } from 'lodash';
 import '../../../../DemoPage/aggriddemo.css';
-import { AdaptableBlotterOptions } from '@adaptabletools/adaptableblotter/types';
+import {
+  AdaptableBlotterOptions,
+  ThemeChangedEventArgs,
+  IAdaptableBlotter,
+} from '@adaptabletools/adaptableblotter/types';
 import json from '../../../../DataSets/Json/NorthwindOrders.json';
 import { HelperAgGrid } from '../../../Helpers/HelperAgGrid';
 import predefinedConfig from './config';
+import { RowStyle } from '@adaptabletools/adaptableblotter/App_Scripts/PredefinedConfig/DesignTimeState/UserInterfaceState';
 
 export default () => {
   let helperAgGrid = new HelperAgGrid();
   helperAgGrid.setUpAgGridLicence();
 
+  var adaptableblotter: IAdaptableBlotter;
   let rowData = JSON.parse(JSON.stringify(json));
 
   const columndefs = helperAgGrid.getBasicNorthwindColumnSchema();
@@ -34,10 +43,47 @@ export default () => {
   };
 
   const blotterOptionsClone = cloneDeep(blotterOptions);
-  new AdaptableBlotter(blotterOptions);
+  adaptableblotter = new AdaptableBlotter(blotterOptions);
+
+  adaptableblotter.api.eventApi.on(
+    'ThemeChanged',
+    (themeChangedEventArgs: ThemeChangedEventArgs) => {
+      listenToThemeChanged(adaptableblotter, themeChangedEventArgs);
+    }
+  );
 
   return {
     predefinedConfig,
     blotterOptions: blotterOptionsClone,
   };
 };
+
+function listenToThemeChanged(
+  adaptableblotter: IAdaptableBlotter,
+  args: ThemeChangedEventArgs
+) {
+  if (args.themeName === 'wimbledon-theme') {
+    let rowStyles: RowStyle[] = [];
+    let evenStyle: RowStyle = {
+      Style: {
+        ForeColor: 'white',
+        BackColor: '#462376',
+        FontWeight: 'Bold',
+      },
+      RowType: 'Even',
+    };
+    let oddStyle: RowStyle = {
+      Style: {
+        ForeColor: 'white',
+        BackColor: '#0e6537',
+        FontStyle: 'Italic',
+      },
+      RowType: 'Odd',
+    };
+    rowStyles.push(evenStyle);
+    rowStyles.push(oddStyle);
+    adaptableblotter.api.userInterfaceApi.setRowStyles(rowStyles);
+  } else {
+    adaptableblotter.api.userInterfaceApi.clearRowStyles();
+  }
+}
