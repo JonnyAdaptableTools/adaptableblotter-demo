46cf630feeff49ea240e9c76ae39254c58b2de95
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2019-11-29 09:13:09 +0000

    updating system status

--- src/client/alerts/aggridsystemstatusdemo/config.ts
+++ src/client/alerts/aggridsystemstatusdemo/config.ts
@@ -1,1 +1,27 @@
-export default {};
+import { PredefinedConfig } from '@adaptabletools/adaptableblotter/types';
+
+export default {
+  Dashboard: {
+    VisibleToolbars: ['Application', 'Layout'],
+  },
+  Application: {
+    ApplicationToolbarButtons: [
+      {
+        Name: 'setInfo',
+        Caption: 'Set Info',
+      },
+      {
+        Name: 'setWarning',
+        Caption: 'Set Warning',
+      },
+      {
+        Name: 'setError',
+        Caption: 'Set Error',
+      },
+      {
+        Name: 'setSuccess',
+        Caption: 'Set Success',
+      },
+    ],
+  },
+} as PredefinedConfig;
--- src/client/alerts/aggridsystemstatusdemo/demo.ts
+++ src/client/alerts/aggridsystemstatusdemo/demo.ts
@@ -14,6 +14,7 @@ import { AdaptableBlotterOptions } from '@adaptabletools/adaptableblotter/types'
 import json from '../../../../DataSets/Json/NorthwindOrders.json';
 import { HelperAgGrid } from '../../../Helpers/HelperAgGrid';
 import predefinedConfig from './config';
+import { ApplicationToolbarButtonClickedEventArgs } from '@adaptabletools/adaptableblotter/App_Scripts/Api/Events/BlotterEvents';
 
 export default () => {
   let helperAgGrid = new HelperAgGrid();
@@ -38,6 +39,40 @@ export default () => {
   let adaptableblotter = new AdaptableBlotter(blotterOptions);
   adaptableblotter.api.systemStatusApi.setErrorSystemStatus(
     'Server about to restart'
+  );
+
+  adaptableblotter.api.eventApi.on(
+    'ApplicationToolbarButtonClicked',
+    (
+      applicationToolbarButtonClickedEventArgs: ApplicationToolbarButtonClickedEventArgs
+    ) => {
+      let buttonName =
+        applicationToolbarButtonClickedEventArgs.data[0].id
+          .applicationToolbarButton.Name;
+
+      switch (buttonName) {
+        case 'setInfo':
+          adaptableblotter.api.systemStatusApi.setInfoSystemStatus(
+            '30 new records added today'
+          );
+          break;
+        case 'setWarning':
+          adaptableblotter.api.systemStatusApi.setWarningSystemStatus(
+            'Server is running slowly'
+          );
+          break;
+        case 'setError':
+          adaptableblotter.api.systemStatusApi.setErrorSystemStatus(
+            'Server is down!'
+          );
+          break;
+        case 'setSuccess':
+          adaptableblotter.api.systemStatusApi.setSuccessSystemStatus(
+            'Everything is fine'
+          );
+          break;
+      }
+    }
   );
 
   return {
