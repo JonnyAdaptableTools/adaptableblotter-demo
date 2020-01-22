0cdf0645b02b36f85217a7febb344b3977b906de
roblotter <roblotter1@gmail.com>
2020-01-22 19:21:05 +0200

    test

--- plugins/next-cache/index.js
+++ plugins/next-cache/index.js
@@ -4,23 +4,23 @@ function netlifyPlugin(conf) {
   return {
     // Plugin Name
     name: 'next-cache',
-    async onGetCache({ utils }) {
-      if (fs.existsSync('./.next')) {
-        return;
-      }
-      await utils.cache.restore('./.next');
-    },
-    // Cache file/directory for future builds.
-    // Does not do anything if:
-    //  - the file/directory does not exist locally
-    //  - the file/directory is already cached and its contents has not changed
-    //    If this is a directory, this includes children's contents
-    async onSaveCache({ utils }) {
-      if (!fs.existsSync('./.next')) {
-        return;
-      }
-      await utils.cache.save('./.next');
-    },
+    // async onGetCache({ utils }) {
+    //   if (fs.existsSync('./.next')) {
+    //     return;
+    //   }
+    //   await utils.cache.restore('./.next');
+    // },
+    // // Cache file/directory for future builds.
+    // // Does not do anything if:
+    // //  - the file/directory does not exist locally
+    // //  - the file/directory is already cached and its contents has not changed
+    // //    If this is a directory, this includes children's contents
+    // async onSaveCache({ utils }) {
+    //   if (!fs.existsSync('./.next')) {
+    //     return;
+    //   }
+    //   await utils.cache.save('./.next');
+    // },
     // Hook into lifecycle
     onEnd: () => {
       console.log('build finished, site deployed! 🚀');
