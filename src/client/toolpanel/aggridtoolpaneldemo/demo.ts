ac630505e4e7281678eab499443529cf679abab4
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2020-02-06 09:24:18 +0200

    toolpanel section

--- src/client/toolpanel/aggridtoolpaneldemo/demo.ts
+++ src/client/toolpanel/aggridtoolpaneldemo/demo.ts
@@ -1,16 +1,14 @@
-import Adaptable from '@adaptabletools/adaptable/agGrid';
-import '@adaptabletools/adaptable/index.css';
-import '@adaptabletools/adaptable/themes/dark.css';
-import '@ag-grid-community/all-modules/dist/styles/ag-grid.css';
-import '@ag-grid-community/all-modules/dist/styles/ag-theme-balham.css';
-import '@ag-grid-community/all-modules/dist/styles/ag-theme-balham-dark.css';
-import { cloneDeep } from 'lodash';
+import raw from 'raw.macro';
+
 import '../../../../DemoPage/aggriddemo.css';
-import { AdaptableOptions } from '@adaptabletools/adaptable/types';
+
 import json from '../../../../DataSets/Json/NorthwindOrders.json';
 import { HelperAgGrid } from '../../../Helpers/HelperAgGrid';
-import predefinedConfig from './config';
-import { AllEnterpriseModules } from '@ag-grid-enterprise/all-modules';
+
+import init from './code';
+import { GridReadyEvent } from '@ag-grid-community/all-modules';
+const code = raw('./code.ts');
+
 export default () => {
   let helperAgGrid = new HelperAgGrid();
   helperAgGrid.setUpAgGridLicence();
@@ -19,27 +17,22 @@ export default () => {
 
   const columndefs = helperAgGrid.getBasicNorthwindColumnSchema();
 
-  const gridOptions = helperAgGrid.getGridOptions(columndefs, rowData, false);
-  gridOptions.sideBar = true;
-  gridOptions.modules = AllEnterpriseModules;
+  const { adaptableOptions, adaptableApi } = init(columndefs, rowData);
 
-  const adaptableOptions: AdaptableOptions = {
-    primaryKey: 'OrderId',
-    userName: 'Demo User',
-    adaptableId: 'Tool Panel Demo',
-    userInterfaceOptions: {
-      showAdaptableToolPanel: true,
-    },
-    vendorGrid: gridOptions,
-    predefinedConfig: predefinedConfig,
+  adaptableOptions.vendorGrid.onGridReady = function(
+    gridReady: GridReadyEvent
+  ) {
+    gridReady.columnApi!.autoSizeAllColumns();
+    setTimeout(() => gridReady.columnApi!.autoSizeAllColumns(), 1);
+
+    gridReady.api!.addEventListener('newColumnsLoaded', function() {
+      gridReady.columnApi!.autoSizeAllColumns();
+    });
+
+    gridReady.api!.openToolPanel('AdaptableToolPanel');
   };
 
-  const adaptableOptionsClone = cloneDeep(adaptableOptions);
-  const adaptableApi = Adaptable.init(adaptableOptions);
-  gridOptions.api!.openToolPanel('AdaptableToolPanel');
-
   return {
-    predefinedConfig,
-    adaptableOptions: adaptableOptionsClone,
+    code,
   };
 };
--- src/client/toolpanel/aggridtoolpanelnamedemo/config.ts
+++
@@ -1,8 +0,0 @@
-import { PredefinedConfig } from '@adaptabletools/adaptable/types';
-
-export default {
-  Dashboard: {
-    DashboardVisibility: 'Hidden',
-    HomeToolbarTitle: 'TooPanel Name Demo',
-  },
-} as PredefinedConfig;
