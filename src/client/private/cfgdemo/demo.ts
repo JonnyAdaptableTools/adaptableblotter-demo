9befcbb5ae3dc7a5e8bf3621d03d210ad4a6081d
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2020-02-06 13:11:43 +0200

    more changes

---
+++ src/client/private/cfgdemo/demo.ts
@@ -0,0 +1,51 @@
+import Adaptable from '@adaptabletools/adaptable/agGrid';
+import '@adaptabletools/adaptable/index.css';
+
+import '@adaptabletools/adaptable/themes/dark.css';
+
+import '@ag-grid-community/all-modules/dist/styles/ag-grid.css';
+import '@ag-grid-community/all-modules/dist/styles/ag-theme-balham.css';
+import '@ag-grid-community/all-modules/dist/styles/ag-theme-balham-dark.css';
+import { cloneDeep } from 'lodash';
+import '../../../../DemoPage/aggriddemo.css';
+import { AdaptableOptions } from '@adaptabletools/adaptable/types';
+
+import json from '../../../../DataSets/Json/cfg.json';
+import { HelperAgGrid } from '../../../Helpers/HelperAgGrid';
+import predefinedConfig from './config';
+import { CFGHelperAgGrid } from './CFGHelperAgGrid';
+
+export default () => {
+  let helperAgGrid = new HelperAgGrid();
+  helperAgGrid.setUpAgGridLicence();
+  let cfgHelperAgGrid = new CFGHelperAgGrid();
+
+  let rowData = JSON.parse(JSON.stringify(json));
+
+  const columndefs = cfgHelperAgGrid.getCFGDataSchema();
+
+  const gridOptions = helperAgGrid.getGridOptions(columndefs, rowData);
+  gridOptions.floatingFilter = true;
+  gridOptions.defaultColDef = {
+    resizable: true,
+    filter: true,
+    sortable: true,
+  };
+
+  const adaptableOptions: AdaptableOptions = {
+    primaryKey: 'Order ID',
+    userName: 'Demo User',
+    adaptableId: 'CFG Demo',
+
+    vendorGrid: gridOptions,
+    predefinedConfig: predefinedConfig,
+  };
+
+  const adaptableOptionsClone = cloneDeep(adaptableOptions);
+  Adaptable.init(adaptableOptions);
+
+  return {
+    predefinedConfig,
+    adaptableOptions: adaptableOptionsClone,
+  };
+};
