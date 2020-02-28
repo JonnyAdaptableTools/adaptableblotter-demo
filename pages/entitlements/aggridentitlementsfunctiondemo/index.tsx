8e19f3abadda8a7a536a8111c34040ed113a1747
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2020-02-28 12:34:29 +0200

    nonsense

--- pages/entitlements/aggridentitlementsfunctiondemo/index.tsx
+++ pages/entitlements/aggridentitlementsfunctiondemo/index.tsx
@@ -5,45 +5,48 @@ export default () => {
   return (
     <DynamicDemoPage
       demo={import(
-        '../../../src/client/entitlements/aggridentitlementslistdemo'
+        '../../../src/client/entitlements/aggridentitlementsfunctiondemo'
       )}
-      pageTitle={'Entitlements List Demo'}
+      pageTitle={'Entitlements Function Demo'}
       description={
         <div>
           <p>
-            One way to provide Entitlements in Predefined Config is through a
-            'hard-coded' list via the <b>FunctionEntitlements</b> property.
+            Another way to provide Entitlements in Predefined Config is via a
+            Function (the <b>EntitlementLookUpFunction</b> property).
           </p>
           <p>
-            This allows you to provide a list of Entitlements - essentially a{' '}
-            <b>FunctionName</b> and an <b>AccessLevel</b> (either <i>Full</i>,{' '}
-            <i>ReadOnly</i> or <i>Hidden</i>).
+            This allows you to use an external permissioning or entitlment
+            server or to do external lookups rather than provide a hard-coded
+            list.
           </p>
           <p>
-            Any Function not given an Entitlement automatically takes the Access
-            Level of 'Full'; however this can be changed through setting the{' '}
-            <b>DefaultAccessLevel</b> property (see{' '}
+            The function will be run each time an Entitlement is required. It
+            receives the id of the AdapTable instance, the current user, and the
+            Function and it returns an AccessLevel.
+          </p>
+          <p>
+            If the function returns nothing, the Entitlement is given the
+            default Access Level of 'Full' (however this can be changed through
+            setting the <b>DefaultAccessLevel</b> property, see{' '}
             <a href="./aggridentitlementsdefaultdemo">Default Demo</a>
             ).
           </p>
           <p>
-            In this example we have created 3 Entitlements (one for each
-            AccessLevel):
+            In this example the function does 3 things:
             <ul>
               <li>
-                AdvancedSearch - set to <i>Hidden</i> with the result that the
-                Advanced Search Toolbar, ToolPanel and Menu Option are not
-                available anywhere in AdapTable and cannot be accessed by users.
+                For a list of editing-based Functions it returns an AccessLevel
+                of <i>Hidden</i> (presumably it wants a non-editable grid).
               </li>
               <li>
-                Layout - set to <i>ReadOnly</i> with the result that the Layout
-                Toolbar (and menu option) is visible and Layouts can be
-                selected, but they cannot be created, edited or deleted.
+                For a list of other functions it mimics a call to an external
+                Permissions Service (which returns an AccessLevel of{' '}
+                <i>ReadOnly</i>)
               </li>
               <li>
-                Export - set to <i>Full</i> (not strictly required as its the{' '}
-                <i>DefaultAccessLevel</i> but here for display purposes) which
-                means it can be used without restriction.
+                For all other AdapTable functions it does nothing meaning that
+                they will use the Entitlement set by the DefaultAccessLevel
+                property.
               </li>
             </ul>
           </p>
