c5a13bb57df7c6d59313e0554257c29b74ab31b5
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2020-03-26 11:03:18 +0200

    getting there

--- pages/events/searchchangedeventdemo/index.tsx
+++ pages/events/searchchangedeventdemo/index.tsx
@@ -4,7 +4,7 @@ import DynamicDemoPage from '../../../src/Helpers/DynamicDemoPage';
 export default () => {
   return (
     <DynamicDemoPage
-      demo={import('../../../src/client/events/aggridsearchchangedeventdemo')}
+      demo={import('../../../src/client/events/searchchangedeventdemo')}
       pageTitle={'Search Changed Demo'}
       description={
         <div>
