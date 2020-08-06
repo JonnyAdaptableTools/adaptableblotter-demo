19f6f1a1add02071e77fa7ca416b38ce55030ada
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2020-08-06 09:51:49 +0300

    tidying up questions

--- pages/gridgurus/latestquestionsdemo/index.tsx
+++ pages/gridgurus/latestquestionsdemo/index.tsx
@@ -39,18 +39,42 @@ export default () => {
       <p className="gridGuruQuestion">
         <b>Should we filter on the Server or the Client?</b>
         <br />
-        The short answer is that depends entirely on your requirements. <br />
-        What we would say is that <b>only</b> do server searching and filtering
-        if you have to. We see plently of clients who only have 100,000 (or
+        The short answer is that depends entirely on your requirements and is
+        not something we can answer without knowing more about your particular
+        use case. <br />
+        But, as general advice: <b>only</b> do server searching and filtering if
+        you really have to. We see plently of clients who only have 100,000 (or
         fewer) rows in their dataset and yet made the decision to do server
         searching, with all the additional complexity that brings. <br />
-        In our view that is mistaken; modern browsers like Chrome are capable to
-        easily managing datasets of that size and performing instantaneous
-        filtering and sorting. So do use the server if you have to, but we would
-        advise you first to check if you can do everything you need on the
-        server, and only if you cannot then do enable server filtering.
+        In our view that is mistaken; modern browsers are capable of easily
+        managing datasets of that size and performing instantaneous filtering
+        and sorting. So, by all means, do use the server if you have to, but we
+        would advise you first to check if you can do everything you need on the
+        server, and only if you cannot then to enable server filtering.
       </p>
       <h3>ag-Grid Questions</h3>
+      <p className="gridGuruQuestion">
+        <b>
+          We set <i>enableValue</i> to 'false' for a column but it still shows
+          an aggregated value when grouping - we assumed this would remove it?
+        </b>
+        <br />
+        That is expected ag-Grid behaviour: <i>enableValue</i> (and{' '}
+        <i>enablePivot</i> for pivoting and <i>enableRowGroup</i> for grouping)
+        are <b>UI instructions</b>.<br />
+        This means that if these are set to 'false', the User cannot change the
+        behaviour at run-time (i.e. in your case, create a new aggregation or
+        drop it into the 'Values' section in the Column sidebar).
+        <br />
+        However if the column's <i>ColDef</i> definition has the 'aggFunc'
+        property already set (which we assume yours does) then it will display
+        that aggregated value in grouped rows.
+      </p>{' '}
+      <p className="gridGuruQuestion">
+        <b>How do we always make the column menu icon appear?</b>
+        <br />
+        Set the <b>suppressMenuHide</b> property in GridOptions to <i>true.</i>
+      </p>{' '}
       <p className="gridGuruQuestion">
         <b>Is it possible to show aggregated totals at the top of a group?</b>
         <br />
@@ -67,28 +91,6 @@ export default () => {
         ).
       </p>
       <p className="gridGuruQuestion">
-        <b>How do we always make the column menu icon appear?</b>
-        <br />
-        Set the <b>suppressMenuHide</b> property in GridOptions to <i>true.</i>
-      </p>
-      <p className="gridGuruQuestion">
-        <b>
-          We set <i>enableValue</i> to 'false' for a column but it still shows
-          an aggregated value when grouping - we assumed this would remove it?
-        </b>
-        <br />
-        That is expected ag-Grid behaviour: <i>enableValue</i> (and{' '}
-        <i>enablePivot</i> for pivoting and <i>enableRowGroup</i> for grouping)
-        are <b>UI instructions</b>.<br />
-        This means that if these are set to 'false' the User cannot change the
-        behaviour at run-time (e.g. create a new aggregation or drop it into the
-        'Values' section in the Column sidebar).
-        <br />
-        However if the column's <i>ColDef</i> definition has the 'aggFunc'
-        property already set (which we assume yours does) then it will display
-        that aggregated value in grouped rows.
-      </p>
-      <p className="gridGuruQuestion">
         <b>Should we use Master / Detail or Tree View?</b>
         <br />
         In our view it depends on your data structure. If the 'sub' rows are
@@ -97,8 +99,8 @@ export default () => {
         <br />
         If they are linked conceptually but not intrinsically in your data set
         then Master / Detail is more suitable. <br />
-        Note also that in a Tree View, row groping are pivoting are not
-        available.
+        Be aware when making your choice, that a Tree View has additional
+        restrictions, e.g. row groping are pivoting are not available.
       </p>
     </MainPage>
   );
