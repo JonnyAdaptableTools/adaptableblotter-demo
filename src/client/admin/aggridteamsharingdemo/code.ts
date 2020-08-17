2523eebb49729a84901ce0cbe13fe595c08eb6fe
jonnyadaptabletools <adaptabletools@JonnyAdaptableMacBook.local>
2020-08-17 19:24:17 +0300

    fixing orderid for team sharing demo

--- src/client/admin/aggridteamsharingdemo/code.ts
+++ src/client/admin/aggridteamsharingdemo/code.ts
@@ -51,7 +51,7 @@ export default async (columnDefs: any[], rowData: any[]) => {
   };
 
   const adaptableOptions: AdaptableOptions = {
-    primaryKey: 'tradeId',
+    primaryKey: 'OrderId',
     userName: 'Demo User',
     adaptableId: 'Team Sharing Demo',
     predefinedConfig: demoConfig,
