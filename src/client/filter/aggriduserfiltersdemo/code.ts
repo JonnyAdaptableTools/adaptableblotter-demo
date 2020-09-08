dda98c78c6a22d5339d36b12ed9d013cad446c20
jonnyadaptabletools <adaptabletools@JonnyAdaptableMacBook.local>
2020-09-08 10:21:01 +0300

    fixing build

--- src/client/filter/aggriduserfiltersdemo/code.ts
+++ src/client/filter/aggriduserfiltersdemo/code.ts
@@ -1,8 +1,8 @@
-import '../aggridcustompredicatesdemo/node_modules/@adaptabletools/adaptable/index.css';
-import '../aggridcustompredicatesdemo/node_modules/@adaptabletools/adaptable/themes/dark.css';
-import '../aggridcustompredicatesdemo/node_modules/@ag-grid-community/all-modules/dist/styles/ag-grid.css';
-import '../aggridcustompredicatesdemo/node_modules/@ag-grid-community/all-modules/dist/styles/ag-theme-balham.css';
-import '../aggridcustompredicatesdemo/node_modules/@ag-grid-community/all-modules/dist/styles/ag-theme-balham-dark.css';
+import '@adaptabletools/adaptable/index.css';
+import '@adaptabletools/adaptable/themes/dark.css';
+import '@ag-grid-community/all-modules/dist/styles/ag-grid.css';
+import '@ag-grid-community/all-modules/dist/styles/ag-theme-balham.css';
+import '@ag-grid-community/all-modules/dist/styles/ag-theme-balham-dark.css';
 import Adaptable from '@adaptabletools/adaptable/agGrid';
 import { GridOptions } from '@ag-grid-community/all-modules';
 import {
