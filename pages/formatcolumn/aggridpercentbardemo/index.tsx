57ee1ca18c92f5d83779a8f7ba3a41642962f357
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2021-07-11 21:35:46 +0300

    trying to trigger build

--- pages/formatcolumn/aggridpercentbardemo/index.tsx
+++ pages/formatcolumn/aggridpercentbardemo/index.tsx
@@ -14,26 +14,27 @@ export default () => {
             column.
           </p>
           <p>
-            Percent Bar can be created in one of 2 ways: using <b>Ranges</b> or
-            a <b>Column Comparison</b>
-          </p>
-          <p>
-            Ranges will colour the cell depending on which Range the cell value
-            falls in - this allows a traffic light effect for example; there is
-            no limit on the number of Ranges.{' '}
-          </p>
-          <p>
-            A Column Comparison will colour the cell based on another Column in
-            the same Row; you can set the MinValue, MaxValue or both to be the
-            name of the other column.
+            A Percent Bar can be created in one of 2 ways:
+            <ul>
+              <li>
+                <b>Ranges</b>: will colour the cell depending on which Range the
+                cell value falls in - this allows a traffic light effect for
+                example; there is no limit on the number of Ranges.{' '}
+              </li>
+              <li>
+                <b>Column Comparison</b>: will colour the cell based on another
+                Column in the same Row; you can set the MinValue, MaxValue or
+                both to be the name of the other column.
+              </li>
+            </ul>
           </p>
           <p>
             Other{' '}
             <a
-              href="https://docs.adaptabletools.com/docs/predefined-config/percent-bar-config"
+              href="https://docs.adaptabletools.com/docs/predefined-config/format-column-config"
               target="_blank"
             >
-              Percent Bar Config
+              Config
             </a>{' '}
             options include a back colour for the Bar, whether to see the cell
             value, and whether to display that value as a percentage of the
